bce1dfb36a640054517cdc2f2bf09566721ee46f
marcomancio <mm.mancinimarco@gmail.com>
2021-01-26 17:13:40 +0100

    3 files modified, 12 added, 12 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,14 @@ import { Component, VERSION } from "@angular/core";
 })
 export class AppComponent {
   name = "Angular " + VERSION.major;
-  public show:boolean = false;
-  public buttonName:any = 'Show';  
+  public show: boolean = false;
+  public buttonName: any = "Show";
 
   toggle() {
     this.show = !this.show;
 
     // CHANGE THE NAME OF THE BUTTON.
-    if (this.show)
-      this.buttonName = "Hide";
-    else
-      this.buttonName = "Show";
+    if (this.show) this.buttonName = "Hide";
+    else this.buttonName = "Show";
   }
 }
