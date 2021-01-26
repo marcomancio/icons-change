0dd020adbb6761a1fd08394a98aee85340e5927d
marcomancio <mm.mancinimarco@gmail.com>
2021-01-26 16:57:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# icons-change
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/icons-change)
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  public show:boolean = false;
+  public buttonName:any = 'Show';  
+
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if (this.show)
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
+  }
+}
